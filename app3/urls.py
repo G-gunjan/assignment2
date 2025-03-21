e0e12e5adb0833534aa87a52686881320f2db502
Gunjan Pandya <145027556+G-gunjan@users.noreply.github.com>
2025-03-22 00:24:08 +0530

    Added multiple apps

--- app3/urls.py
+++ app3/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),  
+    path('services/', views.services, name='services'),  
 ]
